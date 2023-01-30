84a438182c4fd66e284c53b7d78a98a28ff13981
codepoet <jon.wise@jonandnic.com>
2023-01-29 19:58:16 -0500

    Update script that uploads video to server

--- upload-video.sh
+++ upload-video.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 # This script runs on the OBS computer (or wherever the video originated)
 # 	and uploads the most recent recording in a given folder to the server
+# Replace the <things> with your values to make it work
+
+server=<YOURSERVER>
 videodir="/Users/<YOU>/Movies"
 tempfile="/Users/<YOU>/Movies/video.mp4"
-remotefile=<YOU>@<YOURSERVER.COM>:<YOURPATH>.mp4
+remotefile=<YOU>@$server:<YOURPATH>.mp4
 
+# Setup screen
 clear
+#   Mac only
+osascript \
+    -e 'tell application "Terminal"' \
+    -e 'set position of front window to {40, 40}' \
+    -e 'end tell'
 echo
 echo Video Uploader
 echo ==============
 echo
 
 # Check Ready
-if pgrep "OBS" > /dev/null; then
+if pgrep "OBS" &> /dev/null; then
     echo OBS is still running, cannot confirm video is ready.
     echo Stop Streaming and Recording, wait for OBS to encode the video, and shutdown OBS.
     echo Then run this script again.
     echo 
     exit
 fi
+if ping -c 1 $server &> /dev/null; then
+    echo Network ready!
+else
+    echo -e "\033[0;31m*** ERROR ***"
+    echo The web server is unreachable!
+    echo Check connectivity then run this script again.
+    echo -e "\033[0m"
+    exit
+fi
 
 # Cleanup
+sudo killall shutdown &> /dev/null
 echo Preparing to upload...
-sudo killall shutdown
 rm -f $tempfile
 
 # Find most recent video
@@ -53,3 +71,6 @@ echo
 if [ "$1" = "" ]; then
     sudo shutdown -h +30
 fi
+
+sleep 10
+exit
